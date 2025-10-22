4c35d22604e08df32bd2336a2eed5cbfb867828b
eudavidreis-odev <eudavidreis.dev@gmail.com>
2025-10-22 20:43:55 -0300

    feat: adicionar suporte ao armazenamento assíncrono com AsyncStorage

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -1,12 +1,10 @@
 import auth from '@react-native-firebase/auth';
 import firestore, { FirebaseFirestoreTypes } from '@react-native-firebase/firestore';
-import storage from '@react-native-firebase/storage';
 
 // react-native-firebase auto-inicializa usando os arquivos google-services.json / GoogleService-Info.plist
 // Coloque esses arquivos na raiz do projeto (conforme app.json) para inicializar corretamente.
 
 export const firebaseAuth = auth();
 export const db = firestore();
-export const firebaseStorage = storage();
 
 export type FirestoreTimestamp = FirebaseFirestoreTypes.Timestamp;
